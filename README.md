be359fc881b66e231ce4a50bfb4d6033137cfe09
ntenpas-presence <ntenpas@presencewebservices.net>
2020-09-11 16:18:21 -0500

    Updated README to include description of envelope header. Updated gmailer.go to be properly formatted

--- README.md
+++ README.md
@@ -1,36 +1,42 @@
 # gmailer
-Go module that sends email using GMail OAuth
+Go module that sends email using GMail OAuth.
+
+Email envelope header shows email sent from name as the `ReplyTo` address, while the from address is `EmailFrom`:
+`From: reply-to@email.com <email-from@email.com>`
+This is useful in combination with `EmailFrom` set to a no-reply address.
+
+Based on code borrowed from [BinodKafle/gomail](https://github.com/BinodKafle/gomail)
 
 Example usage:
 ```go
 package main
 
 import (
-  "fmt"
-  "github.com/Presence-Web-Services/gmailer"
+	"fmt"
+	"github.com/Presence-Web-Services/gmailer"
 )
 
 func main() {
-  cfg := gmailer.Config{
-    ClientID: "YOUR_CLIENT_ID",
-    ClientSecret: "YOUR_CLIENT_SECRET",
-    AccessToken: "YOUR_ACCESS_TOKEN",
-    RefreshToken: "YOUR_REFRESH_TOKEN",
-    EmailTo: "email_to@email.com",
-    EmailFrom: "email_from@email.com",
-    ReplyTo: "reply-to@email.com",
-    Subject: "Test subject",
-    Body: "This is a test email body... thanks!",
-  }
+	cfg := gmailer.Config{
+		ClientID:     "YOUR_CLIENT_ID",
+		ClientSecret: "YOUR_CLIENT_SECRET",
+		AccessToken:  "YOUR_ACCESS_TOKEN",
+		RefreshToken: "YOUR_REFRESH_TOKEN",
+		EmailTo:      "email-to@email.com",
+		EmailFrom:    "email-from@email.com",
+		ReplyTo:      "reply-to@email.com",
+		Subject:      "Test Subject",
+		Body:         "This is a test email body... thanks!",
+	}
 
-  var srv, err = cfg.Authenticate()
-  if err != nil {
-    fmt.Println("Error occured during authenticate")
-  }
+	var service, err = cfg.Authenticate()
+	if err != nil {
+		fmt.Println("Error occurred during authenticate")
+	}
 
-  err = cfg.Send(srv)
-  if err != nil {
-    fmt.Println("Error occured during send")
-  }
+	err = cfg.Send(service)
+	if err != nil {
+		fmt.Println("Error occurred during send")
+	}
 }
 ```
--- gmailer.go
+++ gmailer.go
@@ -4,64 +4,64 @@ Gmailer allows for OAUTH authentication to GMail and sending of basic email mess
 package gmailer
 
 import (
-  "context"
-  "time"
-  "encoding/base64"
+	"context"
+	"encoding/base64"
+	"time"
 
-  "golang.org/x/oauth2"
-  "golang.org/x/oauth2/google"
-  "google.golang.org/api/gmail/v1"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/option"
 )
 
 // Config is type containing OAuth credentials and email message information
 type Config struct {
-  ClientID string
-  ClientSecret string
-  AccessToken string
-  RefreshToken string
-  EmailTo string
-  EmailFrom string
-  ReplyTo string
-  Subject string
-  Body string
+	ClientID     string
+	ClientSecret string
+	AccessToken  string
+	RefreshToken string
+	EmailTo      string
+	EmailFrom    string
+	ReplyTo      string
+	Subject      string
+	Body         string
 }
 
 // Authenticate authenticates with GMail OAuth, and returns a pointer to gmail.Service when successfully authenticated
 func (config *Config) Authenticate() (gmailService *gmail.Service, err error) {
-  oauthConfig := oauth2.Config{
-    ClientID:     config.ClientID,
-    ClientSecret: config.ClientSecret,
-    Endpoint:     google.Endpoint,
-    RedirectURL:  "http://localhost",
-  }
-  oauthToken := oauth2.Token{
-    AccessToken:  config.AccessToken,
-    RefreshToken: config.RefreshToken,
-    TokenType:    "Bearer",
-    Expiry:       time.Now(),
-  }
+	oauthConfig := oauth2.Config{
+		ClientID:     config.ClientID,
+		ClientSecret: config.ClientSecret,
+		Endpoint:     google.Endpoint,
+		RedirectURL:  "http://localhost",
+	}
+	oauthToken := oauth2.Token{
+		AccessToken:  config.AccessToken,
+		RefreshToken: config.RefreshToken,
+		TokenType:    "Bearer",
+		Expiry:       time.Now(),
+	}
 
-  tokenSource := oauthConfig.TokenSource(context.Background(), &oauthToken)
+	tokenSource := oauthConfig.TokenSource(context.Background(), &oauthToken)
 
-  gmailService, err = gmail.NewService(context.Background(), option.WithTokenSource(tokenSource))
-  return
+	gmailService, err = gmail.NewService(context.Background(), option.WithTokenSource(tokenSource))
+	return
 }
 
 // Send takes a pointer to gmail.Service and is called on a Config type to send an email
 func (config *Config) Send(gmailService *gmail.Service) (err error) {
-  var message gmail.Message
+	var message gmail.Message
 
-  emailTo := "To: " + config.EmailTo + "\r\n"
-  emailFrom := "From: " + config.ReplyTo + "<" + config.EmailFrom + ">" + "\r\n"
-  replyTo := "Reply-To: " + config.ReplyTo + "\r\n"
-  subject := "Subject: " + config.Subject + "\n"
-  mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
-  msg := []byte(emailTo + emailFrom + replyTo + subject + mime + "\n" + config.Body)
+	emailTo := "To: " + config.EmailTo + "\r\n"
+	emailFrom := "From: " + config.ReplyTo + "<" + config.EmailFrom + ">" + "\r\n"
+	replyTo := "Reply-To: " + config.ReplyTo + "\r\n"
+	subject := "Subject: " + config.Subject + "\n"
+	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
+	msg := []byte(emailTo + emailFrom + replyTo + subject + mime + "\n" + config.Body)
 
-  message.Raw = base64.URLEncoding.EncodeToString(msg)
+	message.Raw = base64.URLEncoding.EncodeToString(msg)
 
-  // Send the message
-  _, err = gmailService.Users.Messages.Send("me", &message).Do()
-  return
+	// Send the message
+	_, err = gmailService.Users.Messages.Send("me", &message).Do()
+	return
 }
